1ba54354f71af425a5cc79ea1884b8aed8f1d0db
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-24 18:45:24 +0530

    [shravanthi] - Basic form using html, css, js validation

--- formValidation.js
+++ formValidation.js
@@ -1,7 +1,7 @@
-const text=document.querySelector("#name");
+const text=document.getElementById('name');
 let errorMsg=document.querySelector(".errorMsg");
 text.addEventListener('input',function(){
-    let regexSample=RegExp('^[A-Z]{1}[a-z]{2}$');
+    let regexSample=RegExp('^[A-Z]{1}[a-z]{2,}$');
     if(regexSample.test(text.value)){
         errorMsg.textContent="";
     }
@@ -10,3 +10,14 @@ text.addEventListener('input',function(){
         errorMsg.textContent="Given name is Invalid";
     }
 })
+function validate(){
+    var phoneNumber = document.getElementById('contact').value;
+    var  name= document.getElementById('name').value;
+    var phoneRGEX = RegExp('^[0-9]{2}[0-9]{8}');
+    var regexSample=RegExp('^[A-Z]{1}[a-z]{2,}$');
+    var phoneResult = phoneRGEX.test(phoneNumber);
+    var nameResult = regexSample.test(name);
+    alert("phone:"+phoneResult + "name :"+nameResult);
+    
+    return false;
+  }
